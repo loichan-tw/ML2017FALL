dc130fce275b08855930d95794f01ce24dceba4d
loichan <31984601+loichan-tw@users.noreply.github.com>
2017-10-26 17:11:07 +0800

    Add files via upload

---
+++ hw2/hw2log.py
@@ -0,0 +1,63 @@
+import os, sys
+import numpy as np
+from random import shuffle
+import argparse
+from math import log, floor
+import pandas as pd
+
+def load_data(train_data_path, train_label_path, test_data_path):
+    X_train = pd.read_csv(train_data_path, sep=',', header=0)
+    X_train = np.array(X_train.values)
+    Y_train = pd.read_csv(train_label_path, sep=',', header=0)
+    Y_train = np.array(Y_train.values)
+    X_test = pd.read_csv(test_data_path, sep=',', header=0)
+    X_test = np.array(X_test.values)
+
+    return (X_train, Y_train, X_test)
+def normalize(X_all, X_test):
+    # Feature normalization with train and test X
+    X_train_test = np.concatenate((X_all, X_test))
+    mu = (sum(X_train_test) / X_train_test.shape[0])
+    sigma = np.std(X_train_test, axis=0)
+    mu = np.tile(mu, (X_train_test.shape[0], 1))
+    sigma = np.tile(sigma, (X_train_test.shape[0], 1))
+    X_train_test_normed = (X_train_test - mu) / sigma
+
+    # Split to train, test again
+    X_all = X_train_test_normed[0:X_all.shape[0]]
+    X_test = X_train_test_normed[X_all.shape[0]:]
+    return X_all, X_test
+def sigmoid(z):
+    res = 1 / (1.0 + np.exp(-z))
+    return np.clip(res, 1e-8, 1-(1e-8))
+
+def infer(X_test, output_dir):
+    test_data_size = len(X_test)
+
+    # Load parameters
+    #print('=====Loading Param from %s=====' % save_dir)
+    '''    w = np.loadtxt(os.path.join(save_dir, 'w'))
+    b = np.loadtxt(os.path.join(save_dir, 'b'))
+    w2 = np.loadtxt(os.path.join(save_dir, 'w2'))'''
+
+    w = np.loadtxt( 'logistic_params/w')
+    b = np.loadtxt( 'logistic_params/b')
+    w2 = np.loadtxt('logistic_params/w2')
+    # predict
+    z = (np.dot(X_test, np.transpose(w)) + b + np.dot(np.square(X_test), np.transpose(w2)))
+    y = sigmoid(z)
+    y_ = np.around(y)
+
+ #   print('=====Write output to %s =====' % output_dir)
+  #  if not os.path.exists(output_dir):
+   #     os.mkdir(output_dir)
+    output_path = output_dir
+    with open(output_path, 'w') as f:
+        f.write('id,label\n')
+        for i, v in  enumerate(y_):
+            f.write('%d,%d\n' %(i+1, v))
+
+    return
+X_all, Y_train, X_test1 = load_data(sys.argv[1],sys.argv[2],sys.argv[3])
+X_all, X_test = normalize(X_all, X_test1)
+infer(X_test, sys.argv[4])
