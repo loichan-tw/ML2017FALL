9494d2b0dfa535e8817b996f554064c71381f87a
loichan <31984601+loichan-tw@users.noreply.github.com>
2017-11-17 17:40:29 +0800

    Update hw3_test.sh

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -O my_model 'https://www.dropbox.com/s/vgtxzkvzb3ayauz/my_model.h5?dl=1'
+wget -O my_model.h5 'https://www.dropbox.com/s/vgtxzkvzb3ayauz/my_model.h5?dl=1'
 python3 hw3pretrain.py $1 $2 my_model.h5
 
 
